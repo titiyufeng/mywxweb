edfb6b9d24b2e672481da2b5edf4121473f89371
teo <titiyufeng@126.com>
2018-09-15 17:27:03 +0800

    改变变量名，增加其他商品属性
    改变变量名，增加其他商品属性

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,30 +2,30 @@ Page({
   data: {
     cateItems: [
       {
-        cate_id: 999,
+        cate_id: 0,
         cate_name: "热门",
         ishaveChild: true,
         children:
           [
             {
-              child_id: 10000001,
-              name: '洁面皂',
+              spu_no: 10000001,
+              goods_name: '洁面皂',
+              price: 100,
+              is_display:true,
+              stock_quantity:9999,
+              frozen_quantity:0,
+              is_hot:false,
               image: "http://mz.djmall.xmisp.cn/files/logo/20161208/148117972563.jpg"
             },
             {
-              child_id: 10000002,
-              name: '卸妆',
+              spu_no: 10000002,
+              goods_name: '卸妆',
+              price: 100,
+              is_display: true,
+              stock_quantity: 9999,
+              frozen_quantity: 0,
+              is_hot: false,
               image: "../../resouce/type_select.png"
-            },
-            {
-              child_id: 10000003,
-              name: '洁面乳',
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161208/148117973270.jpg"
-            },
-            {
-              child_id: 10000004,
-              name: '面部祛角质',
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161208/148117981591.jpg"
             }
           ]
       }, {
@@ -35,26 +35,24 @@ Page({
         children:
           [
             {
-              child_id: 10000001,
-              name: '洁面皂',
-              //image: "http://mz.djmall.xmisp.cn/files/logo/20161208/148117972563.jpg"
+              spu_no: 10000001,
+              goods_name: '洁面皂',
+              price: 100,
+              is_display: true,
+              stock_quantity: 9999,
+              frozen_quantity: 0,
+              is_hot: false,
               image: "../../resouce/1/10000001.png"
             },
             {
-              child_id: 10000002,
-              name: '卸妆',
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161207/148110444480.jpg",
+              spu_no: 10000002,
+              goods_name: '卸妆',
+              price: 100,
+              is_display: true,
+              stock_quantity: 9999,
+              frozen_quantity: 0,
+              is_hot: false,
               image: "../../resouce/type_select.png"
-            },
-            {
-              child_id: 10000003,
-              name: '洁面乳',
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161208/148117973270.jpg"
-            },
-            {
-              child_id: 10000004,
-              name: '面部祛角质',
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161208/148117981591.jpg"
             }
           ]
       },
@@ -65,44 +63,24 @@ Page({
         children:
           [
             {
-              child_id: 20000001,
-              name: '气垫bb',
+              spu_no: 20000001,
+              goods_name: '气垫bb',
+              price: 100,
+              is_display: true,
+              stock_quantity: 9999,
+              frozen_quantity: 0,
+              is_hot: false,
               image: "http://mz.djmall.xmisp.cn/files/logo/20161212/14815381301.jpg"
             },
             {
-              child_id: 20000002,
-              name: '修容/高光',
+              spu_no: 20000002,
+              goods_name: '修容/高光',
+              price: 100,
+              is_display: true,
+              stock_quantity: 9999,
+              frozen_quantity: 0,
+              is_hot: false,
               image: "http://mz.djmall.xmisp.cn/files/logo/20161212/14815381411.jpg"
-            },
-            {
-              child_id: 20000003,
-              name: '遮瑕',
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161212/148153815181.jpg"
-            },
-            {
-              child_id: 20000004,
-              name: '腮红',
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161212/148153815759.jpg"
-            },
-            {
-              child_id: 20000005,
-              name: '粉饼',
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161212/148153816983.jpg"
-            },
-            {
-              child_id: 20000006,
-              name: '粉底',
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161212/148153817721.jpg"
-            },
-            {
-              child_id: 20000007,
-              name: '蜜粉/散粉',
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161212/148153819354.jpg"
-            },
-            {
-              child_id: 20000008,
-              name: '隔离霜',
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161215/148179053369.jpg"
             }
           ]
       },
@@ -113,24 +91,24 @@ Page({
         children:
           [
             {
-              child_id: 30000001,
-              name: '淡香水EDT',
+              spu_no: 30000001,
+              goods_name: '淡香水EDT',
+              price: 100,
+              is_display: true,
+              stock_quantity: 9999,
+              frozen_quantity: 0,
+              is_hot: false,
               image: "http://mz.djmall.xmisp.cn/files/logo/20161213/14815978910.jpg"
             },
             {
-              child_id: 30000002,
-              name: '浓香水EDP',
+              spu_no: 30000002,
+              goods_name: '浓香水EDP',
+              price: 100,
+              is_display: true,
+              stock_quantity: 9999,
+              frozen_quantity: 0,
+              is_hot: false,
               image: "http://mz.djmall.xmisp.cn/files/logo/20161213/148159789883.jpg"
-            },
-            {
-              child_id: 30000003,
-              name: '香体走珠',
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161213/14815979307.jpg"
-            },
-            {
-              child_id: 30000004,
-              name: '古龙香水男士的最爱',
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161213/148159765589.jpg"
             }
           ]
       },
@@ -141,7 +119,7 @@ Page({
         children: []
       }
     ],
-    curNav: 999,
+    curNav: 0,
     curIndex: 0
   },
 
