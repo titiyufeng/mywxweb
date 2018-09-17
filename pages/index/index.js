57e72f1a1d6d3e4c08d58b2eb8059eb904d45cd0
teo <teo@futunn.com>
2018-09-17 21:04:17 +0800

    调整明细页面图片展示，并增加商品
    调整明细页面图片展示，并增加商品

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,15 +8,15 @@ Page({
         children:
           [
             {
-              spu_no: 10000001,
-              goods_name: '洁面皂',
-              price: 100,
-              is_display:true,
-              stock_quantity:9999,
-              frozen_quantity:0,
-              is_hot:false,
-              goods_detail:"商品详细描述信息",
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161208/148117972563.jpg"
+              spu_no: 60000001,
+              goods_name: "Lamer海蓝之谜奇迹面霜30ml",
+              price: 998,
+              is_display: true,
+              stock_quantity: 9999,
+              frozen_quantity: 0,
+              is_hot: false,
+              goods_detail: "海蓝之谜最经典的面霜全球十大面霜之首  这款神奇面霜被时代周刊评为“最值得拥有的奢侈品之一” 。神奇的滋润保湿效果，它是敏感肌换季救星，能够拯救了总是水肿的隔夜脸，它是拯救痘痘脸的终极法宝，更是敏感肌首选，五颗星推荐‼️神奇的再生能力。",
+              image: "../../resouce/60000001/60000001.jpg"
             },
             {
               spu_no: 10000002,
@@ -147,6 +147,26 @@ Page({
             is_hot: false,
             goods_detail: "海蓝之谜最经典的面霜全球十大面霜之首  这款神奇面霜被时代周刊评为“最值得拥有的奢侈品之一” 。神奇的滋润保湿效果，它是敏感肌换季救星，能够拯救了总是水肿的隔夜脸，它是拯救痘痘脸的终极法宝，更是敏感肌首选，五颗星推荐‼️神奇的再生能力。",
             image: "../../resouce/60000001/60000001.jpg"
+          }, {
+            spu_no: 60000002,
+            goods_name: "伊思晶钻金蜗牛霜 60ml",
+            price: 198,
+            is_display: true,
+            stock_quantity: 9999,
+            frozen_quantity: 0,
+            is_hot: false,
+            goods_detail: "想要白滑美肌，但脸上又有红血丝、又有痘印未好怎么办？选一瓶全效修复面霜就可以啦！It’s Skin伊思晶钻美肌蜗牛活肤面霜之所以能成为第一蜗牛霜品牌，创下全球每6秒售出一瓶的佳绩，就是因为此品能兼具美白、淡斑、去疤痕、痘印、收毛孔之功效，亦能够使受损的细胞即时修复并促成再生，让皮肤变得更加滋润亮白有光泽，经过多种专业皮肤检验及测试认证，一瓶蜗牛面霜已能有效提供皮肤最根本营养，蜗牛液萃取物（21%）当中的高浓度黏蛋白及软骨素亦除有助滋润肌肤外， 更能快速修复皮肤伤口、红印及疤痕，减缓刺激，保养角质，抗皱淡斑，镇定肌肤。",
+            image: "../../resouce/60000002/60000002.jpg"
+          }, {
+            spu_no: 60000003,
+            goods_name: "黛珂时光活氧抗皱面霜30ml",
+            price: 198,
+            is_display: true,
+            stock_quantity: 9999,
+            frozen_quantity: 0,
+            is_hot: false,
+            goods_detail: "针对因环境压力问题、年龄增长产生的各种肌肤问题 ，保持肌肤的柔软和弹力、改善肌肤、毛孔松弛状态cellgenie面霜LF ：适合任何年纪因为年龄增长导致的松弛的肌肤哦",
+            image: "../../resouce/60000003/60000003.jpg"
           }
         ]
       },
