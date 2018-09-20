d017640fe8984e49f78e36d25d6bc501da2c84f2
teo <teo@futunn.com>
2018-09-20 09:32:57 +0800

    重新规划主图片位置，添加新的商品
    重新规划主图片位置，添加新的商品

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,7 +16,7 @@ Page({
               frozen_quantity: 0,
               is_hot: false,
               goods_detail: "海蓝之谜最经典的面霜全球十大面霜之首  这款神奇面霜被时代周刊评为“最值得拥有的奢侈品之一” 。神奇的滋润保湿效果，它是敏感肌换季救星，能够拯救了总是水肿的隔夜脸，它是拯救痘痘脸的终极法宝，更是敏感肌首选，五颗星推荐‼️神奇的再生能力。",
-              image: "../../resouce/60000001/60000001.jpg",
+              image: "../../resouce/60000001.jpg",
               detail_images:
               [
                 "https://img.alicdn.com/imgextra/i3/2424298091/O1CN0129dj1aD8AuHdq6N_!!2424298091.jpg_760x760Q50s50.jpg",
@@ -33,7 +33,13 @@ Page({
               frozen_quantity: 0,
               is_hot: false,
               goods_detail: "商品详细描述信息",
-              image: "../../resouce/10000002/10000002.png"
+              image: "../../resouce/10000002.png",
+              detail_images:
+                [
+                  "https://img.alicdn.com/imgextra/i3/2424298091/O1CN0129dj1aD8AuHdq6N_!!2424298091.jpg_760x760Q50s50.jpg",
+                  "https://img.alicdn.com/imgextra/i2/2424298091/TB2hE8vXrsTMeJjy1zcXXXAgXXa_!!2424298091.jpg_760x760Q50s50.jpg"
+
+                ]
             }
           ]
       }, {
@@ -51,7 +57,11 @@ Page({
               frozen_quantity: 0,
               is_hot: false,
               goods_detail: "商品详细描述信息",
-              image: "../../resouce/10000001/10000001.png"
+              image: "../../resouce/10000001.png",
+              detail_images:
+                [
+                  
+                ]
             },
             {
               spu_no: 10000002,
@@ -62,7 +72,11 @@ Page({
               frozen_quantity: 0,
               is_hot: false,
               goods_detail: "商品详细描述信息",
-              image: "../../resouce/10000002/10000002.png"
+              image: "../../resouce/10000002.png",
+              detail_images:
+                [
+
+                ]              
             }
           ]
       },
@@ -81,7 +95,11 @@ Page({
               frozen_quantity: 0,
               is_hot: false,
               goods_detail: "商品详细描述信息",
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161212/14815381301.jpg"
+              image: "http://mz.djmall.xmisp.cn/files/logo/20161212/14815381301.jpg",
+              detail_images:
+                [
+
+                ]           
             },
             {
               spu_no: 20000002,
@@ -92,7 +110,11 @@ Page({
               frozen_quantity: 0,
               is_hot: false,
               goods_detail: "商品详细描述信息",
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161212/14815381411.jpg"
+              image: "http://mz.djmall.xmisp.cn/files/logo/20161212/14815381411.jpg",
+              detail_images:
+                [
+
+                ]           
             }
           ]
       },
@@ -111,7 +133,11 @@ Page({
               frozen_quantity: 0,
               is_hot: false,
               goods_detail: "商品详细描述信息",
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161213/14815978910.jpg"
+              image: "http://mz.djmall.xmisp.cn/files/logo/20161213/14815978910.jpg",
+              detail_images:
+                [
+
+                ]           
             },
             {
               spu_no: 30000002,
@@ -122,7 +148,11 @@ Page({
               frozen_quantity: 0,
               is_hot: false,
               goods_detail: "商品详细描述信息",
-              image: "http://mz.djmall.xmisp.cn/files/logo/20161213/148159789883.jpg"
+              image: "http://mz.djmall.xmisp.cn/files/logo/20161213/148159789883.jpg",
+              detail_images:
+                [
+
+                ]           
             }
           ]
       },
@@ -152,7 +182,14 @@ Page({
             frozen_quantity: 0,
             is_hot: false,
             goods_detail: "海蓝之谜最经典的面霜全球十大面霜之首  这款神奇面霜被时代周刊评为“最值得拥有的奢侈品之一” 。神奇的滋润保湿效果，它是敏感肌换季救星，能够拯救了总是水肿的隔夜脸，它是拯救痘痘脸的终极法宝，更是敏感肌首选，五颗星推荐‼️神奇的再生能力。",
-            image: "../../resouce/60000001/60000001.jpg"
+            image: "../../resouce/60000001.jpg",
+            detail_images:
+              [
+                "https://img.alicdn.com/imgextra/i3/2424298091/O1CN0129dj1aD8AuHdq6N_!!2424298091.jpg_760x760Q50s50.jpg",
+                "https://img.alicdn.com/bao/uploaded/i2/TB2yspQaiLaK1RjSZFxXXamPFXa_!!0-rate.jpg",
+                "https://img.alicdn.com/imgextra/i4/TB1FK2LIVXXXXafXXXXXXXXXXXX_!!0-item_pic.jpg",
+                "https://img.alicdn.com/imgextra/i2/2424298091/TB2hE8vXrsTMeJjy1zcXXXAgXXa_!!2424298091.jpg_760x760Q50s50.jpg"
+              ]           
           }, {
             spu_no: 60000002,
             goods_name: "伊思晶钻金蜗牛霜 60ml",
@@ -162,7 +199,15 @@ Page({
             frozen_quantity: 0,
             is_hot: false,
             goods_detail: "想要白滑美肌，但脸上又有红血丝、又有痘印未好怎么办？选一瓶全效修复面霜就可以啦！It’s Skin伊思晶钻美肌蜗牛活肤面霜之所以能成为第一蜗牛霜品牌，创下全球每6秒售出一瓶的佳绩，就是因为此品能兼具美白、淡斑、去疤痕、痘印、收毛孔之功效，亦能够使受损的细胞即时修复并促成再生，让皮肤变得更加滋润亮白有光泽，经过多种专业皮肤检验及测试认证，一瓶蜗牛面霜已能有效提供皮肤最根本营养，蜗牛液萃取物（21%）当中的高浓度黏蛋白及软骨素亦除有助滋润肌肤外， 更能快速修复皮肤伤口、红印及疤痕，减缓刺激，保养角质，抗皱淡斑，镇定肌肤。",
-            image: "../../resouce/60000002/60000002.jpg"
+            image: "../../resouce/60000002.jpg",
+            detail_images:
+              [
+                "https://img.alicdn.com/imgextra/i4/352160544/TB2VSfQib0kpuFjy0FjXXcBbVXa_!!352160544.jpg",
+                "https://img.alicdn.com/imgextra/i4/352160544/TB2O2LXdEdnpuFjSZPhXXbChpXa_!!352160544.jpg",
+                "https://img.alicdn.com/imgextra/i1/352160544/TB21NDbdrJmpuFjSZFBXXXaZXXa_!!352160544.jpg",
+                "https://img.alicdn.com/imgextra/i2/352160544/TB2uLORqR4lpuFjy1zjXXcAKpXa_!!352160544.jpg"
+
+              ]           
           }, {
             spu_no: 60000003,
             goods_name: "黛珂时光活氧抗皱面霜30ml",
@@ -172,7 +217,14 @@ Page({
             frozen_quantity: 0,
             is_hot: false,
             goods_detail: "针对因环境压力问题、年龄增长产生的各种肌肤问题 ，保持肌肤的柔软和弹力、改善肌肤、毛孔松弛状态cellgenie面霜LF ：适合任何年纪因为年龄增长导致的松弛的肌肤哦",
-            image: "../../resouce/60000003/60000003.jpg"
+            image: "../../resouce/60000003.jpg",
+            detail_images:
+              [
+                "https://img.alicdn.com/imgextra/i3/377757379/TB1IRxctyCYBuNkHFCcXXcHtVXa_!!0-item_pic.jpg",
+                "https://img.alicdn.com/imgextra/i2/377757379/TB2OdOSBGSWBuNjSsrbXXa0mVXa_!!377757379.jpg"
+                
+
+              ]           
           }, {
             spu_no: 60000004,
             goods_name: "澳洲绵羊油250g",
@@ -182,7 +234,47 @@ Page({
             frozen_quantity: 0,
             is_hot: false,
             goods_detail: "绵羊油不是羊的脂肪，而是一种从天然羊毛中精炼出来的油脂。绵羊油最大的特点就是“滋润但绝不油腻，吸收好” 。因为绵羊油结构跟人体表面油脂相似，非常适宜人体使用哦。无论面部还是身体都可以用，非常滋润 ！",
-            image: "../../resouce/60000004/60000004.jpg"
+            image: "../../resouce/60000004.jpg",
+            detail_images:
+              [
+                "https://img.alicdn.com/imgextra/i4/47773373/TB21_1ffBcHL1JjSZFBXXaiGXXa_!!47773373.jpg",
+                "https://img.alicdn.com/imgextra/i2/47773373/TB2z6JZfx3IL1JjSZPfXXcrUVXa_!!47773373.jpg",
+                "https://img.alicdn.com/imgextra/i3/47773373/TB2jkBZfx3IL1JjSZPfXXcrUVXa_!!47773373.jpg"
+
+              ]           
+          }, {
+            spu_no: 60000005,
+            goods_name: "HR赫莲娜活颜修护舒缓霜白绷带面霜50ML",
+            price: 2388,
+            is_display: true,
+            stock_quantity: 9999,
+            frozen_quantity: 0,
+            is_hot: false,
+            goods_detail: "犹如被轻柔的隐形美容绷带精心保护一般,肌肤脆弱、微损伤被卓效全频修复。当独特的奢华绵密乳霜交织舒缓复方香调轻抚肌肤，护肤体验随惊艳触感层层铺开。「绷带面霜」的卓越修护力真正从根源改善脆弱肤质——亲眼见证肌肤抵御力显著增强，愈加健康强韧、柔软弹润,紧致光采、宛若新生。卓效修复各类肌肤脆弱、微损伤 ️卓越修复力,适用于日常因各种原因导致的肌肤脆弱与不适",
+            image: "../../resouce/60000005.jpg",
+            detail_images:
+              [
+                "https://img.alicdn.com/imgextra/i4/420250512/O1CN011FeXEzt9SV21uda_!!420250512.jpg",
+                "https://img.alicdn.com/imgextra/i2/420250512/O1CN011FeXF5Lha02GZcu_!!420250512.jpg",
+                "https://img.alicdn.com/imgextra/i2/420250512/O1CN011FeXF5qOmOhH7ck_!!420250512.jpg"
+
+              ]
+          }, {
+            spu_no: 60000006,
+            goods_name: "HR赫莲娜（极之至美）至美溯颜菁华霜滋养霜50ml",
+            price: 1479,
+            is_display: true,
+            stock_quantity: 9999,
+            frozen_quantity: 0,
+            is_hot: false,
+            goods_detail: "丰润细腻的乳霜质地，顺滑易抹，即刻被肌肤吸收，蕴含BIO-SAP植物精华微囊，全效抗御5大肌肤老化现象：皱纹，松弛，晦暗，色斑，缺水。同时阻隔外界对肌肤的侵害。丰润的乳霜质地令使用非凡舒适，用后肌肤光滑细致，无可媲美，老化痕迹显著淡化，肌肤回复完美平滑，紧实，光彩，匀净，润泽💫。辅以按摩手法，效果最佳，肌肤焕然一新，绽放年轻神采💫。",
+            image: "../../resouce/60000006.jpg",
+            detail_images:
+              [
+                "https://img.alicdn.com/imgextra/i1/42428748/TB2Be91HkOWBuNjSsppXXXPgpXa_!!42428748.jpg",
+                "https://img.alicdn.com/imgextra/i1/42428748/TB2IgaMHhSYBuNjSspjXXX73VXa_!!42428748.jpg"
+
+              ]
           }
         ]
       },
