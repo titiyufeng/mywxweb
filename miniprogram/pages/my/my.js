6df9cd1d5efb4adc754d07024bc1582f51ee2d6d
teo <titiyufeng@126.com>
2018-10-01 13:08:24 +0800

    完成个人信息页面、增加其他页面
    完成个人信息页面、增加其他页面

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -7,16 +7,28 @@ Page({
    */
   data: {
     menuitems: [
-      { text: '账号信息', url: '../userinfo/userinfo', icon: '../../resouce/my/my_19_19.png', tips: '' },
-      { text: '我的订单', url: '../borrowbook/borrowbook?status=N', icon: '../../resouce/my/my_12_12.png', tips: '' },
-      { text: '收货地址', url: '../borrowbook/borrowbook?status=F', icon: '../../resouce/my/my_25_25.png', tips: '' }
+      { text: '个人信息', url: './userinfo/userinfo', icon: '../../resouce/my/userinfo.png', tips: '' },
+      { text: '我的订单', url: './order/order', icon: '../../resouce/my/oder.png', tips: '' },
+      { text: '收货地址', url: './address/address', icon: '../../resouce/my/address.png', tips: '' }
       // { text: '待归还', url: '../borrowbook/borrowbook?status=Y', icon: '../../images/usermenu/huan.png', tips: '' },
       // { text: '个人喜好', url: '../favorcate/favorcate', icon: '../../images/usermenu/favor.png', tips: '' },
-    ]
+    ],
+    userinfo: wx.getStorageSync("userinfo")
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+  },
+
+  /**
+   * 获取用户信息
+   */
+  bindGetUserInfo: function (e) {
+    var userinfo = e.detail.userInfo+
+    wx.setStorageSync("userinfo", userinfo)
+    this.setData({
+      userinfo: userinfo
+    })
   }
 })
