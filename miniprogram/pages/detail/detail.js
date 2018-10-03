5120d919b2c039b658c8bb0bd536c843f16eea25
teo <teo@futunn.com>
2018-10-03 17:10:02 +0800

    商品详情页改版
    商品详情页改版

--- miniprogram/pages/detail/detail.js
+++ miniprogram/pages/detail/detail.js
@@ -1,54 +1,135 @@
-// pages/detail/detail.js
+// pages/detail/detail.js 
 var util = require("../../utils/util.js");
 var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    goods_no: "",
-    goods_detail: "",
-    goods_name: "",
-    detail_images: ""
+    detail_images: [],
+    interval: 3000,
+    duration: 800,
+    goods_infos: {
+      goods_no: 1,
+      goods_name: '',
+      goods_price: 0.00,
+      goods_stock: '',
+      goods_detail: '',
+      parameter: '125g/个',
+      service: '不支持退货'
+    },
+    num: 1,
+    totalNum: 0,
+    hasCarts: false,
+    curIndex: 0,
+    show: false,
+    scaleCart: false
+  },
+
+  onLoad: function (options) {
+    
+    var goods_no = options.goods_no*1
+    var cate_id = parseInt(goods_no / 10000000)
+    
+    var detail_images =[]
+    for (var i = 1; i < 6; i++) {
+      detail_images.push("cloud://mywxweb-e946c5.6d79-mywxweb-e946c5/goods_images/" + cate_id + "/" + goods_no + "/" + goods_no + "0" + i + ".jpg")
+    } 
+
+    var that = this
+    app.dbconn.collection('goods_datas').where({
+      goods_no: goods_no,
+      is_display: true
+    }).get({
+      success: function (res) {
+        console.log(res.data.length)
+        if (res.data.length == 0) { //如果没有找到商品，提示商品已经下架
+          wx.showModal({
+            content: '商品已经下架！',
+            showCancel: false,
+            success: function (res) {
+              if (res.confirm) {
+                wx.navigateBack({
+                  // delta: 2
+                })
+              }
+            }
+          })
+        } else {
+          var goods_infos = {
+            goods_no: res.data[0].goods_no,
+            goods_name: res.data[0].goods_name,
+            goods_price: res.data[0].goods_price,
+            goods_stock: '有货',
+            goods_detail: res.data[0].goods_detail
+          }
+          that.setData({
+            goods_infos: goods_infos,
+            detail_images:detail_images
+          })
+        }
+      }
+    })
+
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * 点击加号增加数量
    */
-  onLoad: function(options) {
-    var imgs = new Array()
-    imgs.push("cloud://mywxweb-e946c5.6d79-mywxweb-e946c5/others/wechat.jpg") //加入微信联系图片
-    if (imgs.length < 3) {
-      var cate_id = 0;
-      cate_id = parseInt(options.goods_no / 10000000)
-      for (var i = 1; i < 6; i++) {
-        imgs.push("cloud://mywxweb-e946c5.6d79-mywxweb-e946c5/goods_images/" + cate_id + "/" + options.goods_no + "/" + options.goods_no + "0" + i + ".jpg")
-      }
-    }
+  addCount() {
+    let num = this.data.num;
+    num++;
     this.setData({
-      goods_no: options.goods_no,
-      goods_detail: options.goods_detail,
-      goods_name: options.goods_name,
-      detail_images: imgs,
-      wechat_name: app.globalData.wechat_name,
-      wechat_id: app.globalData.wechat_id
+      num: num
+    })
+  },
+
+
+  /**
+   * 添加至购物车
+   */
+  addToCart() {
+    const self = this;
+    const num = this.data.num;
+    let total = this.data.totalNum;
+
+    self.setData({
+      show: true
+    })
+    setTimeout(function () {
+      self.setData({
+        show: false,
+        scaleCart: true
+      })
+      setTimeout(function () {
+        self.setData({
+          scaleCart: false,
+          hasCarts: true,
+          totalNum: num + total
+        })
+      }, 200)
+    }, 300)
+
+  },
+
+  bindTap(e) {
+    const index = parseInt(e.currentTarget.dataset.index);
+    this.setData({
+      curIndex: index
     })
   },
 
   /**
    * 预览明细页图片
    */
-  imgYu: function(event) {
+  imgYu: function (event) {
     console.log(event.currentTarget.dataset.index);
     var index = event.currentTarget.dataset.index;
     var detail_images = this.data.detail_images;
     wx.previewImage({
       current: detail_images[index], //当前图片地址
       urls: detail_images, //所有要预览的图片的地址集合 数组形式
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
     })
   }
+
 })
