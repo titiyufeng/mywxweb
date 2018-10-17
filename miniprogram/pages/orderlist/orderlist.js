34d5fb38ec709965519cf136823a0994ba0db92b
teo <teo@futunn.com>
2018-10-17 10:11:59 +0800

    订单列表页面后台逻辑基本完成
    订单列表页面后台逻辑基本完成

--- miniprogram/pages/orderlist/orderlist.js
+++ miniprogram/pages/orderlist/orderlist.js
@@ -8,7 +8,13 @@ Page({
     listData: []
   },
   onLoad: function() {
-    var status = {"0": "已提交","1": "已确认","2": "已发货","3": "已结款","9": "已撤销"}
+    var status = {
+      "0": "已提交",
+      "1": "已确认",
+      "2": "已发货",
+      "3": "已结款",
+      "9": "已撤销"
+    }
     var openid = wx.getStorageSync("openid")
     var listData
     var that = this
@@ -17,14 +23,28 @@ Page({
     }).get({
       success: function(res) {
         if (res.data.length > 0) {
-          console.log(res.data)
+          console.log(res.data[0])
+          console.log(res.data[1])
           var listData = res.data
           for (var i = 0; i < listData.length; i++) {
             listData[i].create_time = util.formatTime(listData[i].create_time, 'Y-M-D')
-            listData[i].status = status[listData[i].status.toString()]
-            listData[i].commond = "撤销"
+            listData[i].status = status[listData[i].status]            
+
+            //根据订单状态判断是否显示撤销
+            if (listData[i].status == '已提交') {
+              listData[i].commond = true
+            } else {
+              listData[i].commond = false
+            }
+
+            //根据订单状态判断是否显示应付金额
+            if (listData[i].status != '已发货' && listData[i].status != '已结款') {
+              listData[i].total_amout = "--"
+            }else{
+              listData[i].total_amout = listData[i].amout + listData[i].logistics_fee
+            }
           }
-        }else{
+        } else {
           listData = []
         }
         that.setData({
@@ -32,6 +52,39 @@ Page({
         })
       },
       fail: console.error
+    })
+  },
+
+  /**
+   * 撤销订单
+   */
+  cancel: function(e) {
+    var that = this
+    wx.showModal({
+      // title: '撤销',
+      content: '确定要撤销吗？',
+      success: function(res) {
+        if (res.confirm) {
+          app.dbconn.collection('order').doc(e.target.id).update({
+            data: {
+              status:9
+            },
+            success: function(res) {
+              console.log("订单撤销成功")
+              wx.showModal({
+                content: '撤销成功',
+                showCancel: false,
+                success:function(){
+                  that.onLoad()//调用onload方法重新加载页面数据
+                }
+              })
+            },
+            fail: console.error
+          })
+        } else {
+          console.log("用户点击了取消")
+        }
+      }
     })
   }
 })
