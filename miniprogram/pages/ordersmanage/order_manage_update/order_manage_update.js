1a44b6801213c77e1d0b3f5cd385565c55d30a81
teo <teo@futunn.com>
2018-10-29 08:36:26 +0800

    订单修改页面完成
    订单修改页面完成

--- miniprogram/pages/ordersmanage/order_manage_update/order_manage_update.js
+++ miniprogram/pages/ordersmanage/order_manage_update/order_manage_update.js
@@ -5,7 +5,9 @@ Page({
   data: {
     satuts: ["已提交", "已确认", "已发货", "已结款", "已撤销"],
     satutsIndex: 0,
-    order: {}
+    order: {},
+    province: '', //省
+    city: '' //市
   },
   onLoad: function(options) {
     var status = {
@@ -16,7 +18,7 @@ Page({
       "9": "已撤销"
     }
     var that = this
-    var _id = options.id
+    var _id = options._id
     app.dbconn.collection('order').doc(_id).get({
       success: function(res) {
         var order
@@ -24,26 +26,93 @@ Page({
         order.create_time = util.formatTime(order.create_time, 'Y-M-D h:m:s')
         order.status = status[order.status]
         that.setData({
-          order: order
+          order: order,
+          province: order.province, //省
+          city: order.city //市
         })
       },
       fail: console.error
     })
   },
+
+  /**
+   *切换订单状态 
+   */
   satutsChange: function(e) {
     this.setData({
       satutsIndex: e.detail.value
     })
   },
+
   /**
- * 以下两个方法，是选择省与城市的方法
- */
-  handleClick: function () {
+   *修改订单 
+   */
+  formSubmit: function(e) {
+    var _id = e.detail.value._id
+    var status = String(this.data.satutsIndex)
+    var username = e.detail.value.username
+    var mobile = e.detail.value.mobile
+    var logistics_fee = e.detail.value.logistics_fee
+    var logistics_id = e.detail.value.logistics_id
+    var province = this.data.province
+    var city = this.data.city
+    var detail_address = e.detail.value.detail_address
+
+
+    // console.log("_id:" + _id)
+    // console.log("status:" + status)
+    // console.log("username:" + username)
+    // console.log("mobile:" + mobile)
+    // console.log("logistics_fee:" + logistics_fee)
+    // console.log("logistics_id:" + logistics_id)
+    // console.log("province:" + province)
+    // console.log("city:" + city)
+    // console.log("detail_address:" + detail_address)
+
+    app.dbconn.collection('order').doc(_id).update({
+      data: {
+        status: status,
+        username: username,
+        mobile: mobile,
+        logistics_fee: logistics_fee,
+        logistics_id: logistics_id,
+        province: province,
+        city: city,
+        detail_address: detail_address,
+        update_time: Date.parse(new Date()) / 1000
+      },
+      success: function(res) {
+        console.log("修改订单成功，更新记录数为：" + res.stats.updated + "条！")
+        wx.showModal({
+          content: '提交成功！',
+          showCancel: false,
+          success: function(res) {
+            if (res.confirm) {
+              console.log('用户点击确定')
+              // wx.navigateBack({
+              //   // delta: 2
+              // })
+              wx.reLaunch({
+                url: '../ordersmanage'
+              })
+            }
+          }
+        })
+      },
+      fail: console.error
+    })
+
+  },
+
+  /**
+   * 以下两个方法，是选择省与城市的方法
+   */
+  handleClick: function() {
     this.setData({
       isActive: true
     });
   },
-  handleSelect: function (event) {
+  handleSelect: function(event) {
     console.log(event.detail);
     var province = event.detail[0]
     var city = event.detail[1].fullname
