cfee9dab032b14223993c51db74baa176057201f
teo <teo@futunn.com>
2018-11-22 09:43:39 +0800

    购物清单页面第二次
    购物清单页面第二次

--- miniprogram/pages/ordersmanage/purchaselist/purchaselist.js
+++ miniprogram/pages/ordersmanage/purchaselist/purchaselist.js
@@ -1,66 +1,81 @@
 // miniprogram/pages/ordersmanage/purchaselist/purchaselist.js
+
+var util = require("../../../utils/util.js");
+var app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    topNum: 0,
+    cateItems: [], //左侧导航
+    cateItems_goods_list: [], //右侧商品列表
+    curNav: 0,
+    curIndex: 0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
+  /*
+   获取右侧商品列表、微信信息
    */
+  get_right_goodslist: function (id) {
+    const _ = app.dbconn.command
+    var that = this;
+
+    //判断是否是热门tab，如是过热门tab则不需要过滤商品类型，如果不是热门tab需要过滤商品类型
+    if (id != 0) {
+      app.dbconn.collection('goods_datas').where({
+        cate_id: id,
+        is_display: 1,
+        stock_quantity: _.gt(0)
+      }).limit(50).get({
+        success: function (res) {
+          var right_goodslist = res.data
+          that.setData({
+            right_goodslist: right_goodslist
+          })
+        }
+      })
+    } else {
+      app.dbconn.collection('goods_datas').where({
+        is_hot: 1,
+        is_display: 1,
+        stock_quantity: _.gt(0)
+      }).limit(50).get({
+        success: function (res) {
+          var right_goodslist = res.data
+          that.setData({
+            right_goodslist: right_goodslist
+          })
+        }
+      })
+    }
+  },
+
   onLoad: function (options) {
-
+    var cateItems_db = new Array()
+    var that = this;
+    app.dbconn.collection('goods_types').limit(50).get({
+      success: function (res) {
+        var rs_data = res.data
+        cateItems_db = rs_data
+        that.setData({
+          cateItems: cateItems_db
+        })
+      }
+    })
+    this.get_right_goodslist(6)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //事件处理函数
+  switchRightTab: function (e) {
+    // 获取item项的id，和数组的下标值
+    let id = e.target.dataset.id,
+      index = parseInt(e.target.dataset.index);
+    // 把点击到的某一项，设为当前index
+    this.setData({
+      curNav: id,
+      curIndex: index,
+      topNum: this.data.topNum = 0,
+      right_goodslist: [] //先将右侧列表清空，然后重新加载
+    })
+    this.get_right_goodslist(id)
   }
 })
